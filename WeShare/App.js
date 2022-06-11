21f9608142fd8fbbdd4c0b0c2cf023e08e22a3f1
Daniele Ciammaruconi <30376334+Ciamma@users.noreply.github.com>
2022-06-11 16:00:55 +0200

    settato la navbar, inizializzato le pagine della navbar

--- WeShare/App.js
+++ WeShare/App.js
@@ -1,20 +1,54 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { createMaterialBottomTabNavigator } from "@react-navigation/material-bottom-tabs";
+import { NavigationContainer } from "@react-navigation/native";
+import { StyleSheet } from "react-native";
+import { Icon } from "react-native-elements";
+
+import Contacts from "./Screens/contacts";
+import Profile from "./Screens/profile";
+import WeShare from "./Screens/weshare";
 
 export default function App() {
+  const Tab = createMaterialBottomTabNavigator();
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen
+          name="Profilo"
+          component={Profile}
+          options={{
+            tabBarIcon: ({ color, size }) => (
+              <Icon name="home" color={color} size={26} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="WeShare"
+          component={WeShare}
+          options={{
+            tabBarIcon: ({ color, size }) => (
+              <Icon name="share" color={color} size={26} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Contatti"
+          component={Contacts}
+          options={{
+            tabBarIcon: ({ color, size }) => (
+              <Icon name="contacts" color={color} size={26} />
+            ),
+          }}
+        />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
